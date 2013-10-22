dfffcd4d08c3abf621822470b39435491e6ea890
inv-ftw <inv.ftw@gmail.com>
2013-10-23 01:18:32 +0300

    revision

--- 3.rb
+++ 3.rb
@@ -1,13 +1,17 @@
 puts "Chto ty hotel?"
 question = gets.chomp
 
-while question != question.upcase
-	puts "GOVORI GROMCHE, VNUCHEK!"
-	question = gets.chomp
-end
 while question != "POKA"
-	year = 1930 + rand(21)
-	puts 'NET, NI RAZU S ' + year.to_s + ' GODA!'
-	question = gets.chomp
+
+	if question != question.upcase
+		puts "GOVORI GROMCHE, VNUCHEK!"
+		question = gets.chomp
+	else
+		year = 1930 + rand(21)
+		puts 'NET, NI RAZU S ' + year.to_s + ' GODA!'
+		question = gets.chomp
+	end
+	
 end
+
 puts "POKA!"
